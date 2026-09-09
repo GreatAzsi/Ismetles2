fbbde6069e3516cc7975905bec3d168956437de7
GreatAzsi <akospotfiok@gmail.com>
2026-09-09 10:06:07 +0200

    Did the Tasks from 11-14

---
+++ Ismetles2/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ismetles2
+{
+    public class Library
+    {
+        public string Name { get; set; }
+        private List<Book> Books { get; set; }
+
+        public Library(string name)
+        {
+            Name = name;
+            Books = new List<Book>();
+        }
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+        
+    }
+    
+}
--- Ismetles2/Program.cs
+++ Ismetles2/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
-            book1.PageCountSet(200);
+            Book book1 = new Book("Homeland", "R.A Salvatore");
+            book1.PageCountSet(590);
             Book.Describe(book1);
-            Book book2 = new Book("To Kill a Mockingbird", "Harper Lee",-10);
+            Book book2 = new Book("Exile", "R.A Salvatore",598);
             Book.Describe(book2);
-            Book book3 = new Book("1984", "George Orwell", 328);
+            Book book3 = new Book("Sojurn", "R.A Salvatore", 620);
             Book.Describe(book3);
 
             Console.WriteLine(Book.TotalBooks);
@@ -19,6 +19,17 @@
             Console.WriteLine(Book.Borrow(book1));
             Book.Return(book1);
             Console.WriteLine(Book.Borrow(book1));
+
+            Library library = new Library("My Library");
+            library.AddBook(book1);
+            library.AddBook(book2);
+            library.AddBook(book3);
+            Book book4 = new Book("The Crystal Shard", "R.A Salvatore", 777);
+            library.AddBook(book4);
+            Book book5 = new Book("Streams Of Silver", "R.A Salvatore", 632);
+            Book book6 = new Book("The Halfling’s Gem", "R.A Salvatore", 628);
+            library.AddBook(book5);
+            library.AddBook(book6);
 
 
         }
