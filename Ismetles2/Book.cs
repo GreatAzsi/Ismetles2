805ae7584f66b7449abc21b302f400c1910e9a06
GreatAzsi <akospotfiok@gmail.com>
2026-09-09 08:48:36 +0200

    Did the First Seven Tasks

---
+++ Ismetles2/Book.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ismetles2
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        private int PageCount;
+        public bool IsAvailable { get; set; }
+
+        public int PageCountGetSet() 
+        {
+            Console.WriteLine("Please get or set the Pagecount of the Book");
+            string ans = Console.ReadLine();
+            
+            if(ans.ToLower() == "get")
+            {
+                return PageCount;
+            }
+            else if (ans.ToLower() == "set")
+            {
+                Console.WriteLine("Enter new page count:");
+                int newPageCount = int.Parse(Console.ReadLine());
+                if(newPageCount < 0)
+                {
+                    PageCount = 0;
+                    return PageCount;
+                }
+                else
+                {
+                    PageCount = newPageCount;
+                    return PageCount;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 'get' or 'set'.");
+                return PageCountGetSet();
+            }
+        }
+        public Book(string title, string author, int pageCount)
+        {
+            Title = title;
+            Author = author;
+            PageCount = pageCount;
+        }
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author; 
+            PageCount = 0;
+        }
+        public override string ToString()
+        {
+            return $"{Title} by {Author} with the Pagecount of {PageCount}";
+        }
+
+        public static void Describe(Book book)
+        {
+            Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Page Count: {book.PageCount}");
+        }
+        public static bool Islong(Book book)
+        {
+            return book.PageCount > 300;
+        }
+        public static bool Borrow(Book book)
+        {
+            if (book.IsAvailable)
+            {
+                book.IsAvailable = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void Return(Book book)
+        {
+            book.IsAvailable = true;
+        }
+
+    }
+}
---
+++ Ismetles2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ismetles2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
+            book1.PageCountGetSet();
+            Console.WriteLine(book1);
+            Book book2 = new Book("To Kill a Mockingbird", "Harper Lee",-10);
+            Console.WriteLine(book2);
+
+        }
+    }
+}
